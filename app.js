126501907c15194e4085bd88ad979549867bfc0c
camdeardorff <cameron.deardorff@yahoo.com>
2016-10-02 16:13:32 -0400

    Initial Commit

---
+++ app.js
@@ -0,0 +1,23 @@
+
+var express = require('express');
+var app = express();
+
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+app.use(logger('combined'));
+
+app.use(require('./controllers'));
+
+var server = app.listen(process.env.PORT || 3000, function () {
+	var host = 'localhost';
+	var port = server.address().port;
+	console.log('App listening at http://%s:%s', host, port);
+});
+
+module.exports = app;
