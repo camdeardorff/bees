5f666bac79e5a8beff1cd8b437e563bf2ebdf875
camdeardorff <cameron.deardorff@yahoo.com>
2016-10-26 09:59:31 -0400

    object oriented controller using models

--- controllers/soundReport.js
+++ controllers/soundReport.js
@@ -4,7 +4,9 @@
 
 var express = require('express'),
 	router = express.Router(),
-	db = require('../database/database');
+	SoundRecord = require('../models/SoundRecord'),
+	IntervalRecord = require('../models/IntervalRecord');
+	errorCodes = require('../errorCodes.json');
 
 
 
@@ -17,23 +19,29 @@ router.post('/new', function (req, res) {
 		var time = data.atTime;
 
 		if (sound && time) {
-			time = new Date(time);
-			var selectString = "INSERT INTO `sound_records` (`id`, `sample_time`, `record_time`, `decibels`) VALUES (NULL, ?, CURRENT_TIMESTAMP, ?);";
-			db.getConnection().query(selectString, [time, sound], function (err) {
+
+			console.log("Attempt new sound record");
+			var sr = new SoundRecord(null, time, null, sound);
+			sr.save(function (err, savedSoundResult) {
 				if (err) {
-					console.log(err);
+					console.log("error: ", err);
 					message.success = false;
-					message.error = "There was a problem inserting that into the database";
+					message.error = err;
 				} else {
+					console.log("successfully saved: ", savedSoundResult);
 					message.success = true;
+
 				}
 				res.json(message);
 			});
 		} else {
 			message.success = false;
-			message.error = "Bad Parameters! This api expects 'decibels' : number, and 'atTime' : Date";
+			message.error = errorCodes.badParameters;
 			res.json(message);
 		}
+	} else {
+		message.success = false;
+		res.json(message);
 	}
 });
 
@@ -44,17 +52,23 @@ router.get('/today', function (req, res) {
 	var today = new Date();
 	today.setHours(0,0,0,0);
 
-	console.log("today: ", today);
+	var tomorrow = new Date();
+	tomorrow.setDate(today.getDate() + 1);
+	tomorrow.setHours(0,0,0,0);
 
-	var selectString = "SELECT `sound_records`.`sample_time`, `sound_records`.`decibels`  FROM `sound_records` WHERE `sample_time` > ?";
-	db.getConnection().query(selectString, [today], function (err, rows) {
+	console.log("today: ", today);
+	console.log("tomorrow: ", tomorrow);
+
+	IntervalRecord.betweenDates(today, tomorrow, function (err, records) {
 		if (err) {
-			console.log("error: ", err);
 			message.success = false;
 			message.error = err;
 		} else {
 			message.success = true;
-			message.data = rows;
+			message.records = [];
+			for (var i = 0; i < records.length; i++) {
+				message.records.push(records[i]);
+			}
 		}
 		res.json(message);
 	});
