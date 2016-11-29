c0227d0a8e7e55b27c126803c13a9279da7eeea5
camdeardorff <cameron.deardorff@yahoo.com>
2016-11-28 22:59:46 -0500

    moved to routes configuration

--- routes.js
+++ routes.js
@@ -1,45 +1,24 @@
-// /**
-//  * Created by Cam on 10/8/16.
-//  */
-//
-//
-// exports.setRequestUrl=function(app){
-// 	var user = require('./controllers/user')
-// 		,indexObj = require('./controllers/index')
-// 		,fileObj = require('./controllers/fileSystem')
-// 		,mongoObj = require('./controllers/mongoManagement')
-// 		,articleObj = require('./controllers/article');
-//
-// 	app.get('/', user.login);
-// 	app.post('/onLogin', user.onLogin);
-// 	app.get('/userList', user.userList);
-// 	app.get('/user/addUser', user.addUser);
-// 	app.get('/user/userManager', user.userManager);
-//
-// 	app.post('/index/newContent', indexObj.newContent);
-// 	app.get('/index', indexObj.index);
-// 	app.get('/index/:id', indexObj.viewContect);
-// 	app.get('/index/:id/edit', indexObj.editContect);
-// 	app.post('/index/:id/edit', indexObj.saveContect);
-// 	app.get('/index/:id/delete', indexObj.deleteContectById);
-//
-// 	app.all("/mongo/index",mongoObj.index);
-//
-// 	app.get("/file/*",fileObj.initFileInfo)
-// 	app.get("/fileBrowser/pdf/*",fileObj.initPdf)
-//
-//
-// 	//  app.get("/article/articleManager",articleObj.initManager);
-// 	app.get("/article/articleManager/:articleId?",articleObj.initManager);
-// 	app.get("/article/articleDetail/:articleId?",articleObj.articleDetail);
-// 	app.get("/article/articleItem",articleObj.articleItem);
-// 	app.all("/article/search",articleObj.search)
-//
-// 	app.post("/article/saveArticleType",articleObj.saveArticleType);
-// 	app.post("/article/saveArticleDetail",articleObj.saveArticleDetail);
-// 	app.get("/article/listContext",articleObj.listContextPage);
-// 	app.get("/article/articleTypeAll",articleObj.articleTypeAll);
-//
-//
-//
-// }
+// https://github.com/wchaowu/nodejs/blob/master/NodeSample/routes.js
+
+exports.setRequestUrl = function(app) {
+	var report = require('./controllers/soundReport'),
+		index = require('./controllers/index');
+
+	// front page
+	app.get('/', index.index);
+
+	// new samples
+	app.post('/samples/new', report.new);
+	// get samples
+	app.get('/samples/today/:timeZone', report.today);
+
+	// soon to be depricated
+	// new samples
+	app.post('/soundReport/new', report.new);
+	// get samples
+	app.get('/soundReport/today/:timeZone', report.today);
+
+};
+
+
+
