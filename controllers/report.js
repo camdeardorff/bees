a61e3a1dbd124cfdf9996b900e8d5c17d463a2bd
camdeardorff <cameron.deardorff@yahoo.com>
2016-12-20 20:53:50 -0500

    changed mode of request parameters on range controller

--- controllers/report.js
+++ controllers/report.js
@@ -55,55 +55,65 @@ exports.today = function (req, res, next) {
 
 //'/report/range/from/:fromTime/to/:toTime/inZone/:timeZone'
 
-//times will be in a HH:mm format
+//times will be in a 'valueOf()' format
 
 exports.range = function (req, res, next) {
 	var message = {};
 	var params = req.params;
 
 	if (params) {
-		var from = params["fromTime"];
-		var to = params["toTime"];
+		//
+		var from = new Date(parseInt(params["fromTime"]));
+		var to = new Date(parseInt(params["toTime"]));
 		var timeZone = params["timeZone"].replace("*=SLASH=*", "/");
+
+
+
 
 		console.log("received request for range");
 		console.log("timezone: ", timeZone);
 		console.log("from: ", from, ", to: ", to);
 
-		var start = moment.tz(from, "HH:mm", timeZone);
-		console.log("start: ", start);
-		var end = moment.tz(to, "HH:mm", timeZone);
-		console.log("end: ", end);
+		if (from != null && to != null) {
 
-		IntervalRecord.betweenDates(start.toDate(), end.toDate(), function (err, records) {
-			if (err) {
-				message.success = false;
-				message.error = err;
-			} else {
-				message.success = true;
-				message.records = [];
-				for (var i = 0; i < records.length; i++) {
-					var record = records[i];
+			var start = moment(from).tz(timeZone);
+			console.log("start: ", start);
+			var end = moment(to).tz(timeZone);
+			console.log("end: ", end);
 
-					// simplify everything for the client
-					var bounds = record.interval.boundsValue();
-					delete record.interval;
-					record.range = {
-						from: moment.utc(bounds.from).tz("America/New_York").format("h:mm"),
-						to: moment.utc(bounds.to).tz("America/New_York").format("h:mm")
-					};
-					message.records.push(record);
+			IntervalRecord.betweenDates(start.toDate(), end.toDate(), function (err, records) {
+				if (err) {
+					message.success = false;
+					message.error = err;
+				} else {
+					message.success = true;
+					message.records = [];
+					for (var i = 0; i < records.length; i++) {
+						var record = records[i];
+
+						// simplify everything for the client
+						var bounds = record.interval.boundsValue();
+						delete record.interval;
+						record.range = {
+							from: moment.utc(bounds.from).tz("America/New_York").format("h:mm"),
+							to: moment.utc(bounds.to).tz("America/New_York").format("h:mm")
+						};
+						message.records.push(record);
+					}
 				}
-			}
+				res.json(message);
+				next();
+			});
+		} else {
+			message.success = false;
+			message.error = errorCodes.bad_parameters;
 			res.json(message);
 			next();
-		});
-
-
+		}
 
 	} else {
 		message.success = false;
-		message.error = "Bad params";
+		message.error = errorCodes.bad_parameters;
 		res.json(message);
 		next();
 	}
